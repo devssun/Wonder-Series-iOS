e7074180b8f9933ebb25a56b2b71365923ba6484
devssun <chocochic85@gmail.com>
2021-10-02 13:05:18 +0900

    [Add] App Switcher 예제 프로젝트 추가

---
+++ AppSwitcherTest/AppSwitcherTest/SceneDelegate.swift
@@ -0,0 +1,73 @@
+//
+//  SceneDelegate.swift
+//  AppSwitcherTest
+//
+//  Created by 최혜선 on 2021/10/02.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    var imageView: UIImageView?
+    var blurView: UIVisualEffectView?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        print("sceneDidDisconnect")
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print("sceneDidBecomeActive")
+        if let imageView = blurView {
+            imageView.removeFromSuperview()
+        }
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print("sceneWillResignActive")
+        guard let window = window else {
+            return
+        }
+        
+        // show image
+//        imageView = UIImageView(frame: window.frame)
+//        imageView?.image = UIImage(named: "image")
+//        window.addSubview(imageView!)
+        
+        // show blur effect
+        let effect = UIBlurEffect(style: .regular)
+        blurView = UIVisualEffectView(effect: effect)
+        blurView?.frame = window.frame
+        window.addSubview(blurView!)
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+        print("sceneWillEnterForeground")
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        print("sceneDidEnterBackground")
+    }
+}
+
---
+++ AppSwitcherTest/AppSwitcherTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppSwitcherTest
+//
+//  Created by 최혜선 on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
